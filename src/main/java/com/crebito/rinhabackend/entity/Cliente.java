71ab66cc019264f842de377e2527f49d646eef58
eduardo <eduardodasilvadandolini@gmail.com>
2024-03-01 17:41:39 -0400

    Configurado nginx

--- src/main/java/com/crebito/rinhabackend/entity/Cliente.java
+++ src/main/java/com/crebito/rinhabackend/entity/Cliente.java
@@ -8,6 +8,7 @@ import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
 @Data
 @NoArgsConstructor
